4604853aa063276e00985491b18507e7a56c4544
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-11 11:28:53 +0200

    [DOCS] Food categories completed

--- db.php
+++ db.php
@@ -1,3 +1,14 @@
 <?php 
-    
+
+     $foods = [
+        new Food('Royal Canin Mini Adult', 'https://www.pets-house.it/5767-large_default/royal-canin-mini-adult-800-gr.jpg', 43.99, 'Cane', 545, 'Prosciutto, Riso'),
+        new Food('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 44.99, 'Cane', 600, 'Manzo, Cereali'),
+        new Food('Almo Nature Cat Daily Lattina', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 34.99, 'Gatto', 400, 'Tonno, Pollo, Prosciutto'),
+        new Food('Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 2.95, 'Pesce' , 30, 'Pesci e sottoprodotti di pesci, cereali, lieviti, alghe')
+    ]; 
+
+    $accessories = [
+        new Accessories('Kong Classic', 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 13,49, 'Cane', 'Galleggia e rimbalza', '8,5cm x 10cm'),
+        new Accessories('Topini di peluche Trixie', 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Gatto', 4,99, 'Morbido con codina in corda', '8.5cm x 10cm'),
+    ]
 ?>
--- index.php
+++ index.php
@@ -4,13 +4,7 @@
     require __DIR__.'/models/games.php';
     require __DIR__.'/models/food.php';
     require __DIR__.'/db.php';
-
-    $products = [
-        new Food('Royal Canin Mini Adult', 'https://www.pets-house.it/5767-large_default/royal-canin-mini-adult-800-gr.jpg', 43.99, 'Cane', 545, 'Prosciutto, Riso'),
-        new Food('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 44.99, 'Cane', 600, 'Manzo, Cereali'),
-        new Food('Almo Nature Cat Daily Lattina', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 34.99, 'Gatto', 400, 'Tonno, Pollo, Prosciutto'),
-        new Food('Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 2,95, 'Pesce' , 30, 'Pesci e sottoprodotti di pesci, cereali, lieviti, alghe')
-    ];  
+ 
 ?>
 
 <!DOCTYPE html>
@@ -25,18 +19,35 @@
     <body>
         <div class="container-fluid">
             <div class="row">
-                <?php foreach($products as $product) { ?>
-                <div class="col-4">
+                <div class="col-12 d-flex flex-wrap">
+
+                    <?php foreach($foods as $food) { ?>
                     <div class="card">
-                        <img src="<?php echo $product->image; ?>">
+                        <img src="<?php echo $food->image; ?>">
                         <div class="card-body">
                             <div class="card-title">
-                                <h4><?php echo $product->name ?></h4>
+                                <h4><?php echo $food->name ?></h4>
                             </div>
+                            <p class="card-text"><?php echo $food->category ?></p>
+                            <p class="card-text">Prezzo: <?php echo $food->price ?></p>
+                            <p class="card-text">Peso netto: <?php echo $food->weight ?></p>
+                            <p class="card-text">Ingredienti: <?php echo $food->ingredients ?></p>
                         </div>
                     </div>
+                    <?php } ?>
+
+                    <?php foreach($accessories as $accessory) { ?>
+                     <div class="card">
+                        <img src="<?php echo $accessory->image ?>">
+                        <div class="card-body">
+                            <div class="card-title">
+                                <h4><?php echo $accessory->name ?></h4>
+                            </div>
+                            <p class="card-text"></p>
+                        </div>
+                     </div>   
+                    <?php } ?>
                 </div>
-                <?php } ?>
             </div>
         </div>
     </body>
